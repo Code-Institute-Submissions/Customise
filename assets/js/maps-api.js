0d4d2f28bdd5eb14a753798896747d28ff8754c1
robertc181 <rdancole@gmail.com>
2021-01-30 18:55:04 +0000

    add map id and customise map

---
+++ assets/js/maps-api.js
@@ -0,0 +1,20 @@
+
+
+function initMap() {
+    // The location of Uluru
+    const dublin = { lat: 53.3498, lng:-6.2603 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: dublin,
+      mapId: "d4b44cdc72180b83"
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: dublin,
+      map: map,
+      
+    });
+      
+  }
+
