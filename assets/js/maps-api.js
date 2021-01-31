b98f75f1e2a454d8dca3a327cd288c7f9b2f987f
robertc181 <rdancole@gmail.com>
2021-01-31 00:02:56 +0000

    add form in index.html

--- assets/js/maps-api.js
+++ assets/js/maps-api.js
@@ -5,17 +5,39 @@ function initMap() {
     const dublin = { lat: 53.3498, lng:-6.2603 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 6,
       center: dublin,
       mapId: "d4b44cdc72180b83",
       
     });
-    // The marker, positioned at Uluru
+
     const marker = new google.maps.Marker({
       position: dublin,
       map: map,
       
     });
-      
-  }
 
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+      { lat : 40.785091, lng: -73.968285},
+      { lat : 41.085091, lng: -73.988285},
+      { lat : 40.755091, lng: -73.878285}
+
+    ]
+
+    const markers = locations.map((location, i) => {
+      return new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.length],
+      });
+    });
+   // Add a marker clusterer to manage the markers.
+    new MarkerClusterer(map, markers, {
+     imagePath:
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+ }
+  
+
+  
