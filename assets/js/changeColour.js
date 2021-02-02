e795b9651efbbd8b140ecc5bb6a78341e3ddc4d3
robertc181 <rdancole@gmail.com>
2021-02-02 12:12:37 +0000

    add to change colour js

--- assets/js/changeColour.js
+++ assets/js/changeColour.js
@@ -1,27 +1,38 @@
-debugger;
- localStorage.setItem('colour', 'blue');
 
-//  localStorage.setItem('btncolour', 'bluebtn');
+localStorage.setItem('colour', 'blue');
+
+localStorage.setItem('btncolour', 'bluebtn');
 
 //  localStorage.setItem('bordercolor', 'blue');
 
- function changecolorbtn() {
+function changecolorbtn() {
+    debugger;
+    let currentColour = "." + localStorage.getItem('colour');
 
-        let currentColour = "." + localStorage.getItem('colour', 'btncolour', 'bordercolor');
-    
-        console.log(currentColour);
-    
-        let findClass = document.querySelectorAll(currentColour)
-    
-        console.log(findClass);
-    
-        for (let el of findClass) {
-            
-                let currentColour = localStorage.getItem('colour')
-                
-                 $(el).removeClass(currentColour); $(el).addClass("red");
-                 console.log(el)
-             }
-    
- }
- 
+    let currentBtnColour = "." + localStorage.getItem('btncolour');
+
+    console.log(currentColour, currentBtnColour);
+
+    let findClass = document.querySelectorAll(currentColour)
+
+    let findBtnClass = document.querySelectorAll(currentBtnColour)
+
+    console.log(findClass, findBtnClass);
+
+    for (let el of findClass) {
+
+        let currentColour = localStorage.getItem('colour')
+
+        $(el).removeClass(currentColour); $(el).addClass("red");
+        console.log(el)
+
+    }
+
+    for (let el of findBtnClass) {
+
+        let currentBtnColour = localStorage.getItem('btncolour');
+
+        $(el).removeClass(currentBtnColour); $(el).addClass("redbtn");
+        console.log(el)
+    }
+}
